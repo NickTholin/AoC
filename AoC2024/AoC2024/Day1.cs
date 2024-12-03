f8e48f6367f6651a296c54ce05ab4c1c62b3b069
Nick Tholin <24440637+NickTholin@users.noreply.github.com>
2024-12-02 21:35:42 -0600

    Add Day1 Part1 and Part2  - Location Distance  - Similarity Score

---
+++ AoC2024/AoC2024/Day1.cs
@@ -0,0 +1,67 @@
+﻿namespace AoC2024
+{
+    /// <summary>
+    /// https://adventofcode.com/2024/day/1
+    /// </summary>
+    public class Day1
+    {
+        /// <summary>
+        /// Calculates sum of delta between sorted points of space delimited location pairs
+        /// </summary>
+        /// <param name="twoLists">Expected Input:
+        /// numleft1    numright1
+        /// numleft2    numright2
+        /// Sum of delta of |numleft1 - numright2|</param>
+        /// <returns>Sum of distance between each sorted list element</returns>
+        public static int FindTotalDistance(string twoLists)
+        {
+            var (left, right) = ParseLocations(twoLists);
+            left.Sort();
+            right.Sort();
+
+            var sum = 0;
+
+            for(int i = 0; i <= left.Count -1; i++)
+            {
+                sum += Math.Abs(left[i] - right[i]);
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Calculations score based on sum of number in first list times occurrences in second list.
+        /// </summary>
+        /// <param name="twoLists"></param>
+        /// <returns></returns>
+        public static int FindSimilarityScore(string twoLists)
+        {
+            var (left, right) = ParseLocations(twoLists);
+            //grouping of both lists allows us to iterate over left list with repeated numbers already accummulated 
+            //var leftMap = left.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var rightMap = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            var sum = 0;
+
+            foreach (var location in left)
+            {
+                sum += location * rightMap.GetValueOrDefault(location);
+            }
+
+            return sum;
+        }
+
+        private static (List<int> Left, List<int> Right) ParseLocations(string twoLists)
+        {
+            var lines = twoLists.Split(Environment.NewLine);
+            var allLocations = lines
+                .SelectMany(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+                .Select(int.Parse);
+            var left = allLocations.Where((x, i) => i % 2 == 0).ToList();
+            var right = allLocations.Where((x, i) => i % 2 != 0).ToList();
+            
+            return (left, right);
+        }
+
+    }
+}
