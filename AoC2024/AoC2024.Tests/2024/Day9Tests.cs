08e0a39a3ad8fab36d944d261a03186298fef342
Nick Tholin <nick.tholin@olo.com>
2024-12-13 23:53:55 -0600

    Add Day 9 Part 1 and 2

--- AoC2024/AoC2024.Tests/2024/Day9Tests.cs
+++ AoC2024/AoC2024.Tests/2024/Day9Tests.cs
@@ -5,13 +5,77 @@ namespace AoC.Tests._2024
     public class Day9Tests
     {
 
+        //[Fact]
+        //public void DayPart1Expand()
+        //{
+        //    string input = @"12345";
+
+        //    var num = Day9.CompressForAmphipod(input);
+        //    num.Should().Be("0..111....22222");
+        //}
+
+        //[Fact]
+        //public void DayPart2Expand()
+        //{
+        //    string input = @"12345";
+
+        //    var num = Day9.CompressForAmphipod(input);
+        //    num.Should().Be("022111222");
+        //}
+
+        //[Fact]
+        //public void DayPart2Expand2()
+        //{
+        //    string input = @"12365";
+
+        //    var num = Day9.CompressForAmphipod(input);
+        //    num.Should().Be("022111222");
+        //}
+
+        //[Fact]
+        //public void DayPart2Expand3()
+        //{
+        //    string input = @"12315";
+
+        //    var num = Day9.CompressForAmphipod(input);
+        //    num.Should().Be("022111222");
+        //}
+
+        //[Fact]
+        //public void DayPart2Expand4()
+        //{
+        //    string input = @"2333133121414131402";
+
+        //    var num = Day9.CompressForAmphipod(input);
+        //    num.Should().Be("0099811188827773336446555566");
+        //}
+
+        //[Fact]
+        //public void DayPart2Expand5()
+        //{
+        //    string input = @"414131402";
+
+        //    var num = Day9.CompressForAmphipod(input);
+        //    num.Should().Be("00004111142223333");
+        //}
+
+
         [Fact]
-        public void DayPart1Expand()
+        public void DayPart2Expand3()
         {
             string input = @"12345";
 
-            var num = Day9.CompressForAmphipod(input);
-            num.Should().Be("0..111....22222");
+            var num = Day9.FindChecksumOfCompressedFileSystem(input);
+            num.Should().Be(60);
+        }
+
+        [Fact]
+        public void DayPart2Expand4()
+        {
+            string input = @"2333133121414131402";
+
+            var num = Day9.FindChecksumOfCompressedFileSystem(input);
+            num.Should().Be(1928);
         }
     }
 }
--- AoC2024/AoC2024/2024/Day9.cs
+++ AoC2024/AoC2024/2024/Day9.cs
@@ -1,33 +1,40 @@
-﻿
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace AoC._2024;
 
 public class Day9
 {
-    public static string CompressForAmphipod(string input)
+    public static long FindChecksumOfCompressedFileSystem(string input)
     {
         var disk = CreateDisk(input);
-        return ExpandDisk(disk);
+        var compressed = Compress(disk);
+        var expanded = ExpandDisk(compressed);
+        //return compressed.Aggregate((int acc, DiskSegment segment) => acc + segment.Id * segment.);
+        return expanded
+            .Select((segment, index) => new { segment, index })
+            .Aggregate(0L, (sum, item) => sum + (item.segment.Id * item.index));
     }
 
     public static List<DiskSegment> Compress(List<DiskSegment> source)
     {
         var compressed = new List<DiskSegment>();
-        int freeSpace = source.Sum(x => x.IsFreeSpace ? x.Length : 0);
-
-        var reversedSource = new Stack<DiskSegment>(compressed.AsEnumerable().Reverse());
-
-
-        int i = 0;
+        var totalFreeSpace = source.Sum(x => x.IsFreeSpace ? x.Length : 0);
+        var current = 0;
+        var ranOutOfFiles = false;
+        var lastBlock = source.Count - 1;
 
         do
         {
-            var segment = source[i];
+            var segment = source[current];
+
             if (segment.IsFile)
             {
                 compressed.Add(segment);
+                current++;
+            }
+            else if (segment.Length == 0)
+            {
+                current++;
             }
             else
             {
@@ -35,32 +42,152 @@ public class Day9
 
                 while (freeSpaceLength > 0)
                 {
-                    var segmentFromBack = reversedSource.Peek();
+                    var segmentFromBack = source[lastBlock];
+
+                    if (segmentFromBack.IsFreeSpace)
+                    {
+                        totalFreeSpace -= segmentFromBack.Length;
+                        lastBlock--;
+                        continue;
+                    }
+
+                    // if the source file is the one we are trying to place then there are available open spaces but no more files to condense.
+                    if (current >= lastBlock)
+                    {
+                        ranOutOfFiles = true;
+                        break;
+                    }
+
                     if (segmentFromBack.IsFile)
                     {
-                        //Only pop if we have enough space to fill.  That way we can split files from the back across multiple openings
-                        if (segmentFromBack.Length >= freeSpaceLength)
+                        //If the file is too big to fit in the free space, split it
+                        if (segmentFromBack.Length > freeSpaceLength)
                         {
+                            compressed.Add(new DiskSegment(segmentFromBack.Id, freeSpaceLength));
 
+                            segmentFromBack.Length -= freeSpaceLength;
+                            totalFreeSpace -= freeSpaceLength;
+                            freeSpaceLength = 0;
+                            source.RemoveAt(current);
+                            lastBlock--;
+                        }
+                        else
+                        {
+                            //Only pop if we have enough space to fill.  That way we can split files from the back across multiple openings
+                            compressed.Add(new DiskSegment(segmentFromBack.Id, segmentFromBack.Length));
+                            freeSpaceLength -= segmentFromBack.Length;
+                            lastBlock--;
+
+                            if (freeSpaceLength == 0)
+                                current++; // move onto next file segment in source
+
+                            totalFreeSpace -= segmentFromBack.Length;
                         }
                     }
                     else
                     {
-                        reversedSource.Pop(); // discard freespace
+                        lastBlock--;
+                        totalFreeSpace -= segmentFromBack.Length;
                     }
+
+                    //flush last file segment by appending it to the end if there are no more open segments.
+                    if (totalFreeSpace == 0 && segmentFromBack.Length > 0)
+                        compressed.Add(new DiskSegment(segmentFromBack.Id, segmentFromBack.Length));
                 }
             }
 
-
-        } while (freeSpace > 0);
+        } while (totalFreeSpace > 0 && !ranOutOfFiles);
 
         return compressed;
     }
 
+    public static List<DiskSegment> CompressDefrag(List<DiskSegment> source)
+    {
+        var compressed = new List<DiskSegment>();
+        var totalFreeSpace = source.Sum(x => x.IsFreeSpace ? x.Length : 0);
+        var current = 0;
+        var ranOutOfFiles = false;
+        var lastBlock = source.Count - 1;
 
+        do
+        {
+            var segment = source[current];
 
-    // For troubleshooting
-    public static string ExpandDisk(List<DiskSegment> disk)
+            if (segment.IsFile)
+            {
+                compressed.Add(segment);
+                current++;
+            }
+            else if (segment.Length == 0)
+            {
+                current++;
+            }
+            else
+            {
+                var freeSpaceLength = segment.Length;
+
+                while (freeSpaceLength > 0)
+                {
+                    var segmentFromBack = source[lastBlock];
+
+                    if (segmentFromBack.IsFreeSpace)
+                    {
+                        totalFreeSpace -= segmentFromBack.Length;
+                        lastBlock--;
+                        continue;
+                    }
+
+                    // if the source file is the one we are trying to place then there are available open spaces but no more files to condense.
+                    if (current >= lastBlock)
+                    {
+                        ranOutOfFiles = true;
+                        break;
+                    }
+
+                    if (segmentFromBack.IsFile)
+                    {
+                        //If the file is too big to fit in the free space, split it
+                        if (segmentFromBack.Length > freeSpaceLength)
+                        {
+                            compressed.Add(new DiskSegment(segmentFromBack.Id, freeSpaceLength));
+
+                            segmentFromBack.Length -= freeSpaceLength;
+                            totalFreeSpace -= freeSpaceLength;
+                            freeSpaceLength = 0;
+                            source.RemoveAt(current);
+                            lastBlock--;
+                        }
+                        else
+                        {
+                            //Only pop if we have enough space to fill.  That way we can split files from the back across multiple openings
+                            compressed.Add(new DiskSegment(segmentFromBack.Id, segmentFromBack.Length));
+                            freeSpaceLength -= segmentFromBack.Length;
+                            lastBlock--;
+
+                            if (freeSpaceLength == 0)
+                                current++; // move onto next file segment in source
+
+                            totalFreeSpace -= segmentFromBack.Length;
+                        }
+                    }
+                    else
+                    {
+                        lastBlock--;
+                        totalFreeSpace -= segmentFromBack.Length;
+                    }
+
+                    //flush last file segment by appending it to the end if there are no more open segments.
+                    if (totalFreeSpace == 0 && segmentFromBack.Length > 0)
+                        compressed.Add(new DiskSegment(segmentFromBack.Id, segmentFromBack.Length));
+                }
+            }
+
+        } while (totalFreeSpace > 0 && !ranOutOfFiles);
+
+        return compressed;
+    }
+
+    public static string ExpandDiskToString(List<DiskSegment> disk)
     {
         var expandedDisk = new StringBuilder();
         //does not print nicely if ID > 9
@@ -68,10 +195,23 @@ public class Day9
         {
             expandedDisk.Append(
                 segment.IsFile
-                ? String.Concat(Enumerable.Repeat(segment.Id, segment.Length)) 
+                ? string.Concat(Enumerable.Repeat(segment.Id, segment.Length)) 
                 : new string('.', segment.Length));
         }
         return expandedDisk.ToString();
+    }
+
+    // For troubleshooting
+    public static List<DiskSegment> ExpandDisk(List<DiskSegment> disk)
+    {
+        var expandedDisk = new List<DiskSegment>();
+
+        foreach (var diskSegment in disk)
+        {
+            expandedDisk.AddRange(Enumerable.Repeat(new DiskSegment(diskSegment.Id, diskSegment.Length), diskSegment.Length));
+        }
+
+        return expandedDisk;
     }
 
     public static List<DiskSegment> CreateDisk(string compressedFile)
@@ -97,10 +237,10 @@ public class Day9
         return disk;
     }
 
-    public readonly struct DiskSegment(int Id, int Length)
+    public class DiskSegment(int Id, int Length)
     {
         public int Id { get; } = Id; 
-        public int Length { get; } = Length; 
+        public int Length { get; set; } = Length; 
         public bool IsFreeSpace { get; init; } = false;
         public bool IsFile => !IsFreeSpace;
 
